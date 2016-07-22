677b6f500dec6a4daeadc3fa2f0ce7a121a3d3db
SenShreya <shren17@gmail.com>
2016-07-22 18:28:35 +0530

    Update BasicMath.java

--- BasicMath.java
+++ BasicMath.java
@@ -4,7 +4,7 @@ class BasicMath {
     public static void main(String[] args) {
            int a=10, b=20;
         int res,c;
-        System.out.println("Arithmetic operations");
+        System.out.println("Arithmetic");
         System.out.println("Addition:a+b= "+(a+b));
         System.out.println("Subtraction:a-b= "+(a-b));
         System.out.println("Multiplication:a*b= "+(a*b));
